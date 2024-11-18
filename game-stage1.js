29465f8fd3c16ac70474493a9829955ff717082e
hamlsy <dltmddud1122@naver.com>
2024-11-18 20:05:48 +0900

    fix

--- game-stage1.js
+++ game-stage1.js
@@ -1,5 +1,5 @@
-// import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
-const { Bodies, Body, Engine, Events, Render, Runner, World } = Matter;
+import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
+// const { Bodies, Body, Engine, Events, Render, Runner, World } = Matter;
 import { GRADES_BASE } from "./grades";
 import "./dark.css";
 
